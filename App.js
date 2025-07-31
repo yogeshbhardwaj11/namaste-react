0b39052921282cd5486c4cce89e9349c91db165e
yogeshbhardwaj11 <yogeshbhardwaj11@gmail.com>
2025-07-31 23:38:03 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,22 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    'div', { id: 'parent' }, [  
-        React.createElement(
-            'div', { id: "child1"}, [
-                React.createElement('h1', {}, 'This is namaste React'),
-                React.createElement('h2', {}, 'I am h2 tag')
-            ]
-        ),
-        React.createElement(
-            'div', { id: "child2"}, [
-                React.createElement('h1', {}, 'I am h1 tag'),
-                React.createElement('h2', {}, 'I am h2 tag')
-            ]
-        )]
-);
-console.log(parent);
+const titleText = "Welcome to React";
+
+// React Function Component
+// React Component is the normal javascript function which returns jsx expression
+// jsx - html-like or xml-like syntax inside javascript
+// jsx => React.createElement => React Element => html
+const TitleComponent = () => {
+    // Any javascript expression can be executed within the {} inside Component
+    return(
+        <h1>
+            {titleText}
+        </h1>
+
+    );
+}
+
+// React Element syntax for single line
+// const titleElement = <h1>{titleText}/h1>;
+
+// React Element syntax for multiple lines.
+const titleElement = (
+    // <h1>
+    //     {titleText}
+    // </h1>
+
+    // {TitleComponent()}
+    // <TitleComponent></TitleComponent>
+    <TitleComponent />
+)
+
+// React Component can be render inside React Element
+// React Component can be render inside another React Component
+// React Element can be rendered inside another React Element
+// React Element can be rendered inside another React Component
+const ContentComponent = () => {
+    return(
+        <div>
+            <TitleComponent />
+            <span>Welcome to React!</span>
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(parent);
+root.render(<ContentComponent />);
