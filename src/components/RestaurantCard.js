4378b27c0ecfb8daefc32107032e9c8dbda3b18a
yogeshbhardwaj11 <yogeshbhardwaj11@gmail.com>
2025-08-03 21:37:31 +0530

    episode-6.1

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,7 +2,7 @@ import { CDN_URL } from "../utils/constants";
 
 // we can also do destructuring on the fly => const RestaurantCard = ({name, cuisine})
 const RestaurantCard = (props) => {
-	const { name, cloudinaryImageId, cuisines, avgRating } = props.restaurantData;
+	const { name, cloudinaryImageId, cuisines, avgRating, costForTwo, sla } = props.restaurantData;
     return (
         <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
             <img
@@ -12,7 +12,9 @@ const RestaurantCard = (props) => {
             />
             <h3>{name}</h3>
             <h4>{cuisines.join(",")}</h4>
-            <h4>{avgRating} ratings</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{sla?.slaString} mins</h4>
         </div>
     );
 }
