64f9c7513b5a4d6b6193bbc0d70386eaebfe005c
yogeshbhardwaj11 <yogeshbhardwaj11@gmail.com>
2025-08-05 10:48:06 +0530

    episode-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,39 @@
-const About= () => {
-  return (
-		<div className="about">
-			<h1>About Us</h1>
-		</div>
-  );
+import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
+class About extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	componentDidMount(){}
+
+	render(){
+		return (
+			<div className="about">
+				<h1>About Us</h1>	
+				<UserClass
+					name="Yogesh Bhardwaj"
+					location="Delhi NCR"
+					contact="@yogeshbhardwaj11"
+				/>
+			</div>
+  	);
+	}
 }
+
+// const About= () => {
+//   return (
+// 		<div className="about">
+// 			<h1>About Us</h1>	
+// 			<UserClass
+// 				name="Yogesh Bhardwaj"
+// 				location="Delhi NCR"
+// 				contact="@yogeshbhardwaj11"
+// 			/>
+// 		</div>
+//   );
+// }
 
 export default About;
---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const User = (props) => {
+	const {name, location, contact} = props;
+	const [count] = useState(0);
+	const [count2] = useState(1);
+
+	return(
+		<div className="user">
+			<h2>Count: {count}</h2>
+			<h2>Count2: {count2}</h2>
+			<h2>Name: {name}</h2>
+			<h2>location: {location}</h2>
+			<h2>Contact: {contact}</h2>
+		</div>
+	)
+}
+
+export default User;
